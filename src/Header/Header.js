c1127dc7a079765647c25218521e2a89301634b9
Triana Cerda <trianaRcerda@gmail.com>
2021-08-17 14:57:44 -0700

    added getAllAnimals fetch function

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -10,7 +10,7 @@ class Header extends Component {
             <NavLink exact to="/">
               All The Animals
             </NavLink>
-            <NavLink to="/animals/:id">Animal Profile</NavLink>
+            {/* <NavLink to="/animals/:id">Animal Profile</NavLink> */}
             <NavLink to="/create">Add To Da Pack</NavLink>
           </div>
         </header>
