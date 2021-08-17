27f142c0283f76e33757b43823c99246c301e05c
Triana Cerda <trianaRcerda@gmail.com>
2021-08-17 16:28:07 -0700

    adding links for details page

--- src/AnimalsDetails/AnimalsDetails.js
+++ src/AnimalsDetails/AnimalsDetails.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
+import { getAnimalById } from '../fetch-utils.js';
 
 class AnimalsDetails extends Component {
+    state = {
+        id: 0,
+        name: '',
+        type: 0,
+        snuggly: true,
+    };
+
+    componentDidMount = async () => {
+        const animalsId = this.props.match.params.id;
+        const animalsData = await getAnimalById(animalsId);
+        this.setState({ ...animalsData });
+    };
     render() { 
         return ( 
-            <h1>
-                AnimalsDetails goes here 
-            </h1>
+            <>
+                <h1>{ this.state.name }</h1>
+                <div>this furbaby is snuggly?</div> 
+                <div>{ this.state.type }</div>
+                <div>{ this.state.snuggly }</div> 
+           </>
          );
     }
 }
